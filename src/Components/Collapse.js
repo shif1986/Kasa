79704bdfef4fa35fc197e3798be684a9491165ce
Shifnas <shifnas@ymail.com>
2023-08-25 15:40:36 +0200

    installation collapse, style est debut de consctruction

--- src/Components/Collapse.js
+++ src/Components/Collapse.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "./reusable-ui/Logo";
 import Footer from "./reusable-ui/LogoFooter";
 import "../style/components/_collapse.scss";
 
-
-
-export default function Collapse() {
-  return (
-   <div>
+function Collapse({ title, description }) {
+  // fonction usState
+  const [isOpen, setIsOpen] = useState(false);
+  // let buttonLabel
+  const toggle = () => {
+    // if (isOpen) {
+    //   buttonLabel= "Close"
  
-   </div>
+    // } else {
+ 
+    //   buttonLabel="Open"
+    // }
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <div onClick={toggle}>
+        {" "}
+        <p>{title}</p>
+        {/* il faut que description affiche */}
+        {isOpen ? <button> Close </button> : <button className="arrow-up"> <i class="fa-brands fa-facebook"></i> </button>}
+        {isOpen && <div> {description} </div>}
+      </div>
+    </div>
   );
 }
 
+export default Collapse;
