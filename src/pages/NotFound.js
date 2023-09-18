5f35200e63c2571ca1b761a89b28017ce14401b8
Shifnas <shifnas@ymail.com>
2023-09-18 14:51:13 +0200

    correction sur toutes SCSS

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -5,7 +5,7 @@ import "../style/components/_not-found.scss";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="container" >
       <h1 className="erreur-code" >404</h1>
       <p className="erreur-message">Oups! La page que vous demandez n'existe pas.</p>
       <p className="erreur-home"><Link to="/"> Retourner sur la page d'accuille  </Link></p>
